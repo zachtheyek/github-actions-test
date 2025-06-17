a355d152849dcf1e23df7ced68bc693a3f1a78ce
zachtheyek <xiaoxiyek1.5@gmail.com>
2025-06-17 23:40:18 +0800

    Test: Github Actions <> Watchtower. Display message changed 'Hello World!' -> 'hello, docker!'

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+    res.send('<h1>hello, docker!</h1>')
 })
 
 const PORT = 8080
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
