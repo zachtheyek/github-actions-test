214a17bfac1a959c30b412fa28b5f7b6d05bbd27
zachtheyek <xiaoxiyek1.5@gmail.com>
2025-07-20 03:21:19 +0800

    Test: local + cloud simultaneous deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('<h1>hello, aliens!</h1>')
+    res.send('<h1>hello, CI/CD!</h1>')
 })
 
 const PORT = 8080
